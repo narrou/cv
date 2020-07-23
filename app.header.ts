af1ff073ae169822c1f0948553258aa0a0f9377f
narrou <56250573+narrou@users.noreply.github.com>
2020-07-23 16:34:40 +0200

    Add files via upload

---
+++ app.header.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'header-cv',
+  templateUrl: './app.header.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppHeaderComponent {
+    backgroundHeader = "https://i.ytimg.com/vi/d-zKJCKsoWw/maxresdefault.jpg"
+}
--- app.module.ts
+++ app.module.ts
@@ -4,11 +4,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbdCarouselBasicComponent } from './app.carousel'
+import { AppHeaderComponent } from './app.header';
+import {ProfilePictureComponent} from './app.profilePicture'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgbdCarouselBasicComponent
+    NgbdCarouselBasicComponent,
+    AppHeaderComponent,
+    ProfilePictureComponent
   ],
   imports: [
     BrowserModule,
@@ -16,6 +20,6 @@ import { NgbdCarouselBasicComponent } from './app.carousel'
     NgbModule,
   ],
   providers: [],
-  bootstrap: [AppComponent, NgbdCarouselBasicComponent]
+  bootstrap: [AppComponent, NgbdCarouselBasicComponent, AppHeaderComponent, ProfilePictureComponent]
 })
 export class AppModule { }
