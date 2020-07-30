71f81c38ccb9a43b6850ffdbc7513abdeed7e2f1
narrou <56250573+narrou@users.noreply.github.com>
2020-07-30 16:14:38 +0200

    Delete app.profilePicture.ts

--- app.profilePicture.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'profile-picture',
-    templateUrl: './app.profilePicture.html'})
-export class ProfilePictureComponent {
-  image="https://images.assetsdelivery.com/compings_v2/solargaria/solargaria1707/solargaria170700111.jpg"
-}
